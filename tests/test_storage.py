c7909284326186ba3b52b7b72433865c29d2987f
aschep <schepinsan@gmail.com>
2026-03-05 08:59:33 +0300

    added repo test

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -66,3 +66,43 @@ def test_save_player(get_db):
     assert saved.grip == player.grip
     assert saved.height == player.height
     assert saved.weight == player.weight
+
+
+def test_find_player_by_player_id(get_db):
+    player_repo = PlayerRepository(get_db)
+    team_repo = TeamRepository(get_db)
+
+    team = TeamInfo(
+        "team name",
+        "/teams/sdfsdfsdfsdfsdfsdfsdfsdf_2342348/",
+    )
+    saved_team = team_repo.save(team)
+
+    player = Player(
+        "2342348",
+        "/player/sdfsdfsdfsdfsdfsdfsdfsdf-2013-44-11-2342348/",
+        "Игрок 1",
+        "01-01_2001",
+        "Защ",
+        "Левый",
+        "45",
+        "130",
+        "SuperPuper",
+        4,
+    )
+    saved = player_repo.save(player, saved_team.id)
+    get_db.commit()
+    assert saved.name == player.name
+
+    found = player_repo.find_by_id(player.player_id)
+
+    assert found.name == player.name
+    assert found.player_id == player.player_id
+    assert found.player_url == player.player_url
+    assert found.team_id == saved_team.id
+    assert found.number == player.number
+    assert found.grip == player.grip
+    assert found.height == player.height
+    assert found.weight == player.weight
+    get_db.execute(text("delete from players;"))
+    get_db.execute(text("delete from teams;"))
